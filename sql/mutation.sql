fe14674cea6f399fd2acf0f115b2fe4d64594827
Kohpai <whcwhc78@gmail.com>
2020-10-22 21:23:23 +0200

    :sparkles: close account

--- sql/mutation.sql
+++ sql/mutation.sql
@@ -83,3 +83,20 @@ $$
 
 COMMENT ON FUNCTION public.create_account(name text, type public.account_type, balance money) IS 'create a new account';
 GRANT EXECUTE ON FUNCTION public.create_account(NAME text, TYPE public.account_type, balance money) TO authuser;
+
+CREATE OR REPLACE FUNCTION public.close_account(id integer)
+    RETURNS public.account
+AS
+$$
+DELETE
+FROM public.account AS a
+WHERE a.id = $1
+  AND owner_id = current_setting('jwt.claims.firebase_uid', TRUE)
+RETURNING *
+$$
+    LANGUAGE SQL
+    STRICT
+    SECURITY DEFINER;
+
+COMMENT ON FUNCTION public.close_account(id integer) IS 'close account';
+GRANT EXECUTE ON FUNCTION public.close_account(id integer) TO authuser;
