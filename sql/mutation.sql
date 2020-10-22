f4d453f5c90309c82511f5769cd1f271bf92b83c
Kohpai <whcwhc78@gmail.com>
2020-10-22 21:14:41 +0200

    :sparkles: delete category

--- sql/mutation.sql
+++ sql/mutation.sql
@@ -42,7 +42,7 @@ CREATE OR REPLACE FUNCTION public.create_category(name text)
 AS
 $$
 INSERT INTO public.category (name, owner_id)
-VALUES (name, current_setting('jwt.claims.firebase_uid', TRUE))
+VALUES ($1, current_setting('jwt.claims.firebase_uid', TRUE))
 RETURNING *
 $$
     LANGUAGE SQL
@@ -50,5 +50,21 @@ $$
     SECURITY DEFINER;
 
 COMMENT ON FUNCTION public.create_category(name text) IS 'create a new category to be used in transactions';
+GRANT EXECUTE ON FUNCTION public.create_category(NAME text) TO authuser;
 
-GRANT EXECUTE ON FUNCTION public.create_category(name text) TO authuser;
+CREATE OR REPLACE FUNCTION public.delete_category(id integer)
+    RETURNS public.category
+AS
+$$
+DELETE
+FROM public.category AS c
+WHERE c.id = $1
+  AND owner_id = current_setting('jwt.claims.firebase_uid', TRUE)
+RETURNING *
+$$
+    LANGUAGE SQL
+    STRICT
+    SECURITY DEFINER;
+
+COMMENT ON FUNCTION public.delete_category(id integer) IS 'delete a category';
+GRANT EXECUTE ON FUNCTION public.delete_category(integer) TO authuser;
