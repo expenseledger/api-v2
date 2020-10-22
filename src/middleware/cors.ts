6a730ec94fec2aeea925c9f0cd59bd18fc2c2369
Kohpai <whcwhc78@gmail.com>
2020-10-22 20:10:09 +0200

    :sparkles: make use of cors and auth

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,8 +3,7 @@ import cors from 'cors'
 const allowed = cors({
     origin: [
         'http://localhost:3000',
-        // 'https://the-prince-98130.web.app',
-        // 'https://the-prince-98130.firebaseapp.com',
+        'https://expense-ledger-web.herokuapp.com',
     ],
 })
 
