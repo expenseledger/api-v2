4b8822024cdaaebfe5098c6b1e517ef03f97aaa3
Kohpai <whcwhc78@gmail.com>
2020-10-22 21:20:38 +0200

    :sparkles: create account

--- sql/mutation.sql
+++ sql/mutation.sql
@@ -68,3 +68,18 @@ $$
 
 COMMENT ON FUNCTION public.delete_category(id integer) IS 'delete a category';
 GRANT EXECUTE ON FUNCTION public.delete_category(integer) TO authuser;
+
+CREATE OR REPLACE FUNCTION public.create_account(name text, type public.account_type, balance money)
+    RETURNS public.account
+AS
+$$
+INSERT INTO public.account (name, type, balance, owner_id)
+VALUES ($1, $2, $3, current_setting('jwt.claims.firebase_uid', TRUE))
+RETURNING *
+$$
+    LANGUAGE SQL
+    STRICT
+    SECURITY DEFINER;
+
+COMMENT ON FUNCTION public.create_account(name text, type public.account_type, balance money) IS 'create a new account';
+GRANT EXECUTE ON FUNCTION public.create_account(NAME text, TYPE public.account_type, balance money) TO authuser;
