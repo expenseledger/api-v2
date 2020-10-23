b3d0f0aec142c9e2bc57048332a5819296f0c46b
Kohpai <whcwhc78@gmail.com>
2020-10-23 02:30:08 +0200

    :sparkles: add computed fields

--- sql/index.sql
+++ sql/index.sql
@@ -102,3 +102,21 @@ CREATE POLICY owner_only ON public.transaction TO authuser
                                FROM public.account))
     );
 COMMENT ON TABLE public.transaction IS E'@omit create,update,delete';
+
+CREATE FUNCTION transaction_fromAccount(tx public.transaction)
+    RETURNS public.account AS
+$$
+SELECT *
+FROM public.account
+WHERE id = tx.from_account_id;
+$$ LANGUAGE sql STABLE;
+GRANT EXECUTE ON FUNCTION public."transaction_fromAccount"(tx public.transaction) TO authuser;
+
+CREATE FUNCTION "transaction_toAccount"(tx public.transaction)
+    RETURNS public.account AS
+$$
+SELECT *
+FROM public.account
+WHERE id = tx.to_account_id;
+$$ LANGUAGE sql STABLE;
+GRANT EXECUTE ON FUNCTION public."transaction_toAccount"(tx public.transaction) TO authuser;
