e47ea28727d74a675dd016cce9971136691797f4
Kohpai <whcwhc78@gmail.com>
2020-10-23 02:10:29 +0200

    :sparkles: spend

--- sql/mutation.sql
+++ sql/mutation.sql
@@ -97,3 +97,28 @@ $$
 
 COMMENT ON FUNCTION public.close_account(id integer) IS 'close account';
 GRANT EXECUTE ON FUNCTION public.close_account(id integer) TO authuser;
+
+CREATE OR REPLACE FUNCTION public.spend(amount money, description text, category_id integer, from_account_id integer)
+    RETURNS public.transaction
+AS
+$$
+DECLARE
+    tx public.transaction;
+BEGIN
+    INSERT INTO public.transaction (amount, type, description, category_id, from_account_id, owner_id)
+    VALUES ($1, 'EXPENSE', $2, $3, $4, current_setting('jwt.claims.firebase_uid', TRUE))
+    RETURNING * INTO tx;
+
+    UPDATE
+        public.account
+    SET balance = balance - $1
+    WHERE id = $4;
+
+    RETURN tx;
+END;
+$$
+    LANGUAGE plpgsql
+    STRICT;
+
+COMMENT ON FUNCTION public.spend(amount money, description text, category_id integer, from_account_id integer) IS 'add expense transaction';
+GRANT EXECUTE ON FUNCTION public.spend(amount money, description text, category_id integer, from_account_id integer) TO authuser;
