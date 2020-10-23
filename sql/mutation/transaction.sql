4456bb3c42386d56ef5e28ea6ce016307435c346
Kohpai <whcwhc78@gmail.com>
2020-10-23 02:48:19 +0200

    :sparkles: transfer

--- sql/mutation/transaction.sql
+++ sql/mutation/transaction.sql
@@ -47,3 +47,32 @@ $$
 
 COMMENT ON FUNCTION public.receive(amount money, description text, category_id integer, to_account_id integer) IS 'add income transaction';
 GRANT EXECUTE ON FUNCTION public.receive(amount money, description text, category_id integer, to_account_id integer) TO authuser;
+
+CREATE OR REPLACE FUNCTION public.transfer(amount money, description text, from_account_id integer, to_account_id integer)
+    RETURNS public.transaction
+AS
+$$
+DECLARE
+    tx public.transaction;
+BEGIN
+    INSERT INTO public.transaction (amount, type, description, from_account_id, to_account_id, owner_id)
+    VALUES ($1, 'TRANSFER', $2, $3, $4, current_setting('jwt.claims.firebase_uid', TRUE))
+    RETURNING * INTO tx;
+
+    UPDATE
+        public.account
+    SET balance = balance - $1
+    WHERE id = $3;
+    UPDATE
+        public.account
+    SET balance = balance + $1
+    WHERE id = $4;
+
+    RETURN tx;
+END;
+$$
+    LANGUAGE plpgsql
+    STRICT;
+
+COMMENT ON FUNCTION public.transfer(amount money, description text, from_account_id integer, to_account_id integer) IS 'add transfer transaction';
+GRANT EXECUTE ON FUNCTION public.transfer(amount money, description text, from_account_id integer, to_account_id integer) TO authuser;
