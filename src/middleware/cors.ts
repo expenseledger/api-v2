708bf4eb626f39120ffcd8eaab36632a6c99c6da
oat9002 <oat9002@hotmail.com>
2022-04-18 21:39:04 +0700

    add new url to cor

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,7 +1,11 @@
 import cors from 'cors';
 
 const allowed = cors({
-    origin: ['http://localhost:3000', 'https://expense-ledger-web.herokuapp.com'],
+    origin: [
+        'http://localhost:3000',
+        'https://expense-ledger-web.herokuapp.com',
+        'https://expenseledger.vercel.app/',
+    ],
 });
 
 export default allowed;
