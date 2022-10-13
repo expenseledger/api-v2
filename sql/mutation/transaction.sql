8574f76f82a438a599be8aeb80cc9d82c761e018
oat9002 <oat9002@hotmail.com>
2022-10-13 17:39:28 +0700

    :bug: fix transfer

--- sql/mutation/transaction.sql
+++ sql/mutation/transaction.sql
@@ -93,11 +93,11 @@ BEGIN
     UPDATE
         public.account
     SET balance = balance - $1
-    WHERE id = $3;
+    WHERE id = $4;
     UPDATE
         public.account
     SET balance = balance + $1
-    WHERE id = $4;
+    WHERE id = $5;
 
     RETURN tx;
 END;
