4cba423248bb987ab18605335243df6a97a86d13
oat9002 <oat9002@hotmail.com>
2023-07-02 14:03:46 +0700

    🔒️ account

--- sql/mutation/account.sql
+++ sql/mutation/account.sql
@@ -28,6 +28,22 @@ $$
 COMMENT ON FUNCTION public.close_account(id integer) IS 'close account';
 GRANT EXECUTE ON FUNCTION public.close_account(id integer) TO authuser;
 
+CREATE OR REPLACE FUNCTION public.close_account_v2(id integer)
+    RETURNS public.account
+AS
+$$
+DELETE
+FROM public.account AS a
+WHERE a.id = $1
+AND a.owner_id = current_setting('jwt.claims.firebase_uid', TRUE)
+RETURNING *
+$$
+    LANGUAGE SQL
+    STRICT;
+
+COMMENT ON FUNCTION public.close_account_v2(id integer) IS 'close account';
+GRANT EXECUTE ON FUNCTION public.close_account_v2(id integer) TO authuser;
+
 CREATE OR REPLACE FUNCTION public.update_account(id integer, name text, type public.account_type)
     RETURNS public.account
 AS
@@ -42,3 +58,21 @@ $$
 
 COMMENT ON FUNCTION public.update_account(id integer, name text, type public.account_type) IS 'update account'
 GRANT EXECUTE ON FUNCTION public.update_account(id integer, name text, type public.account_type) TO authuser;
+
+CREATE OR REPLACE FUNCTION public.update_account_v2(id integer, name text, type public.account_type)
+    RETURNS public.account
+AS
+$$
+UPDATE public.account
+SET name = $2, 
+    type = $3
+WHERE id = $1
+AND owner_id = current_setting('jwt.claims.firebase_uid', TRUE)
+
+RETURNING *
+$$
+    LANGUAGE SQL
+    STRICT;
+
+COMMENT ON FUNCTION public.update_accoun_v2(id integer, name text, type public.account_type) IS 'update account'
+GRANT EXECUTE ON FUNCTION public.update_account_v2(id integer, name text, type public.account_type) TO authuser;
