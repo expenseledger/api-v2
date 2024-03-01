1f3a571425975f5ae6c4819a4330e9ad0494ce42
oat9002 <oat9002@hotmail.com>
2024-03-02 00:51:28 +0700

    ♻️

--- sql/query/transaction.sql
+++ sql/query/transaction.sql
@@ -25,7 +25,6 @@ SELECT DISTINCT TO_CHAR(tx.occurred_at , 'YYYY-MM') AS month_year
 FROM public.transaction tx
 WHERE (tx.to_account_id = account_id OR tx.from_account_id = account_id)
 AND tx.owner_id = current_setting('jwt.claims.firebase_uid', TRUE)
-GROUP BY tx.occurred_at
 ORDER BY month_year DESC;
 $$ LANGUAGE sql STABLE;
 GRANT EXECUTE ON FUNCTION public."transaction_month_year_list_by_account_id"(account_id int) TO authuser;
